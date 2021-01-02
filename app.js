0877897d982a4d2d403f4abaf221a5d36bb14b25
LEE-sh1673 <lsh901673@gmail.com>
2021-01-02 17:22:44 +0900

    js mobile setting added.

--- app.js
+++ app.js
@@ -81,6 +81,50 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+
+  // Set up touch events for mobile, etc
+  canvas.addEventListener(
+    "touchstart",
+    function (e) {
+      mousePos = getTouchPos(canvas, e);
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      canvas.dispatchEvent(mouseEvent);
+    },
+    false
+  );
+  canvas.addEventListener(
+    "touchend",
+    function (e) {
+      var mouseEvent = new MouseEvent("mouseup", {});
+      canvas.dispatchEvent(mouseEvent);
+    },
+    false
+  );
+  canvas.addEventListener(
+    "touchmove",
+    function (e) {
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      canvas.dispatchEvent(mouseEvent);
+    },
+    false
+  );
+
+  // Get the position of a touch relative to the canvas
+  function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top,
+    };
+  }
 }
 
 //* Array.from(object): object로부터 array를 만든다.
